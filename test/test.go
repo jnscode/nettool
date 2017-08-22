7c46175e9e444d7bec436fc32001be5c76187f45
jn <jn.life@qq.com>
2017-08-22 23:33:55 +0800

    testing modify

--- test/test.go
+++ test/test.go
@@ -17,44 +17,41 @@ func testPing() {
 	}
 }
 
-func testPing2() {
+func testPingPerform() {
 	tm := time.Now()
 
 	count := 0
+	errCnt := 0
 	ch := make(chan ping.Result)
-	for i := 100; i < 101; i++ {
-		for j := 100; j < 200; j++ {
-			for k := 100; k < 200; k++ {
-				count++
+	for i := 1; i <= 10; i++ {
+		for j := 1; j <= 100; j++ {
+			for k := 1; k <= 100; k++ {
 				ip := fmt.Sprintf("60.%d.%d.%d", i, j, k)
 				param := ping.Param{Addr: ip, DataLen: 32, Segment: true, Timeout: 1}
 				go func() {
 					r, _ := ping.Ping(param)
-					/*if e != nil {
-						fmt.Printf("ping %s ret error %s\n", param.Addr, e.Error())
-					} else {
-						fmt.Printf("ping %s cost %d ms, ttl %d\n", param.Addr, r.Time, r.Ttl)
-					}*/
-
 					ch <- r
 				}()
+
+				count++
 			}
 		}
 	}
 
 	for i := 1; i < count; i++ {
-		<-ch
-		//fmt.Printf("ping ret %v\n", r)
+		r := <-ch
+		if !r.Succ {
+			errCnt++
+		}
 	}
 
 	cost := time.Since(tm) / 1000 / 1000
 
-	fmt.Printf("test completed, ping %d ip cost %d ms\n", count, cost)
+	fmt.Printf("test completed, ping %d ip cost %d ms, error count %d\n", count, cost, errCnt)
 }
 
 func main() {
-	//testPing()
-	testPing2()
+	testPingPerform()
 
 	println("exit main")
 }
